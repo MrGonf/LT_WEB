b75e49d27d23c4038db8b62685f7a71bd661cf18
MrGonf <113083633+MrGonf@users.noreply.github.com>
2025-11-24 21:23:21 +0700

    Add files via upload

---
+++ BT_03_Jpa/java/bt_01/dao/ICategoryDao.java
@@ -0,0 +1,26 @@
+package bt_01.dao;
+
+import java.util.List;
+
+import bt_01.entity.Category;
+
+public interface ICategoryDao {
+
+	int count();
+
+	List<Category> findAll(int page, int pagesize);
+
+	List<Category> findByCategoryname(String catname);
+
+	List<Category> findAll();
+
+	Category findById(int cateid);
+
+	void delete(int cateid) throws Exception;
+
+	void update(Category category);
+
+	void insert(Category category);
+
+
+}
